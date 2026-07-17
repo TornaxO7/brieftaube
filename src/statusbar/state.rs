d090b024f9e149f84dfb0dfbf2af75b666672b9f
TornaxO7 <tornax@proton.me>
2026-07-18 01:15:17 +0200

    refactor(statusbar): add error, warning and info counter

--- src/statusbar/state.rs
+++ src/statusbar/state.rs
@@ -1,13 +1,29 @@
+use ratatui::style::Style;
+
 use crate::Screen;
 
 pub struct State {
     pub(super) screen_name: &'static str,
+    pub(super) errors: usize,
+    pub(super) warnings: usize,
+    pub(super) info: usize,
+
+    pub(super) error_style: Style,
+    pub(super) warning_style: Style,
+    pub(super) info_style: Style,
 }
 
 impl State {
     pub fn new(init_screen: &Screen) -> Self {
         Self {
             screen_name: get_screen_name(init_screen),
+            errors: 0,
+            warnings: 0,
+            info: 0,
+
+            error_style: Style::default().red(),
+            warning_style: Style::default().yellow(),
+            info_style: Style::default().green(),
         }
     }
 
--- src/statusbar/widget.rs
+++ src/statusbar/widget.rs
@@ -1,7 +1,7 @@
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, HorizontalAlignment, Layout, Rect},
-    widgets::{Block, Paragraph, StatefulWidget, Widget},
+    widgets::{Block, Cell, Paragraph, Row, StatefulWidget, Table, Widget},
 };
 
 #[derive(Default)]
@@ -22,20 +22,36 @@ impl StatefulWidget for Statusbar {
         .areas(header_area);
 
         Widget::render(block, area, buf);
-        Widget::render(
-            Paragraph::new("Left").alignment(HorizontalAlignment::Left),
-            left,
-            buf,
-        );
-        Widget::render(
-            Paragraph::new(state.screen_name).alignment(HorizontalAlignment::Center),
-            center,
-            buf,
-        );
+
+        render_error_warning_info_state(left, buf, state);
+        render_screen_name(center, buf, state.screen_name);
+
         Widget::render(
             Paragraph::new("Right").alignment(HorizontalAlignment::Right),
             right,
             buf,
         );
     }
+}
+
+fn render_error_warning_info_state(area: Rect, buf: &mut Buffer, state: &super::State) {
+    let errors = Cell::new(format!("E: {}", state.errors)).style(state.error_style);
+    let warnings = Cell::new(format!("W: {}", state.warnings)).style(state.warning_style);
+    let info = Cell::new(format!("I: {}", state.info)).style(state.info_style);
+
+    let rows = [Row::new([errors, warnings, info])];
+    let widths = [
+        Constraint::Length(5),
+        Constraint::Length(5),
+        Constraint::Length(5),
+    ];
+    Widget::render(Table::new(rows, widths).column_spacing(2), area, buf);
+}
+
+fn render_screen_name(area: Rect, buf: &mut Buffer, name: &str) {
+    Widget::render(
+        Paragraph::new(name).alignment(HorizontalAlignment::Center),
+        area,
+        buf,
+    );
 }
