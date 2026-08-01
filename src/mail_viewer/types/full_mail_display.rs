5cb99981f6b313e6eb63a84f10411ebe08bc9563
TornaxO7 <tornax@proton.me>
2026-08-01 23:16:35 +0200

    refactor(mail-viewer): remove `has_attachment` attribute of `FullMailDisplay`

--- src/mail_viewer/types/full_mail_display.rs
+++ src/mail_viewer/types/full_mail_display.rs
@@ -1,15 +1,13 @@
 use crate::backend::mails::types::{
-    MailData, MailDataAttachment, MailDataHtmlBody, MailDataTextBody, MailId, addresses_to_string,
+    MailData, MailDataAttachment, MailDataHtmlBody, MailDataTextBody, addresses_to_string,
 };
 
 pub struct FullMailDisplay {
-    pub id: MailId,
     pub from: String,
     pub to: String,
     pub cc: String,
     pub subject: String,
     pub received_at: String,
-    pub has_attachment: bool,
 
     pub html_body: Option<MailDataHtmlBody>,
     pub text_body: Option<MailDataTextBody>,
@@ -19,13 +17,11 @@ pub struct FullMailDisplay {
 impl From<MailData> for FullMailDisplay {
     fn from(mail: MailData) -> Self {
         Self {
-            id: mail.id,
             from: addresses_to_string(&mail.from),
             to: addresses_to_string(&mail.to),
             cc: addresses_to_string(&mail.cc),
             subject: mail.subject,
             received_at: mail.received_at.format("%A, %d %B %Y %T").to_string(),
-            has_attachment: mail.has_attachment,
 
             html_body: mail.html_body,
             text_body: mail.text_body,
